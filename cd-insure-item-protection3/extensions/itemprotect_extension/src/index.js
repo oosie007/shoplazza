a547ec98069764a1af2c140683256e915b7c0a29
Georgios Klaros <geoklar@gmail.com>
2026-03-03 10:06:34 +0200

    test

--- cd-insure-item-protection3/extensions/itemprotect_extension/src/index.js
+++ cd-insure-item-protection3/extensions/itemprotect_extension/src/index.js
@@ -3,6 +3,7 @@ import { extend } from 'shoplazza-extension-ui';
 // App URL where checkout-widget.js is served.
 const APP_URL = 'https://shoplazza-nu.vercel.app';
 
+
 const widgetHtml =
   '<div id="cd-insure-widget-root" data-extension="itemprotect" style="margin-top:16px"></div>' +
   `<script>window.CD_INSURE_APP_URL="${APP_URL}";window.SHOPLAZZA_SHOP_DOMAIN=window.location.hostname;</script>` +
